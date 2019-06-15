62a813341311d4efb35b7fb6be5b78c29d41370a
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-15 20:04:10 +0200

    Add variance to Dock component

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,12 +1,31 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-export type DockProps = React.HTMLAttributes<HTMLDivElement>;
+export type DockProps = React.HTMLAttributes<HTMLDivElement> & {
+  variant?: DockVariant;
+};
+
+export const dockStyles = {
+  fill: `
+    width: 100%;
+    height: 100%;
+  `,
+  cover: `
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  `,
+  topRight: `
+    position: absolute;
+    top: 0;
+    right: 0;
+  `
+};
+
+export type DockVariant = keyof typeof dockStyles;
 
 export const Dock = styled.div<DockProps>`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  ${props => dockStyles[props.variant || 'cover']}
 `;
