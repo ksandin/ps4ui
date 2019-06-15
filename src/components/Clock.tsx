8b522e6d13aa9156391d5e48de9e1a2ca4faf8be
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-15 20:26:13 +0200

    Create clock component
    Fixes #3

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,4 +1,15 @@
 import * as React from 'react';
 import { Typography } from './Typography';
+import { useObserver } from 'mobx-react-lite';
+import { timeBehavior } from '../behaviors/timeBehavior';
 
-export const Clock = () => <Typography>20:00</Typography>;
+export const Clock = () =>
+  useObserver(() => {
+    const [time, setTime] = React.useState(new Date());
+    React.useEffect(() => timeBehavior(setTime), []);
+    return (
+      <Typography variant="h3">
+        {time.getHours()}:{time.getMinutes()}
+      </Typography>
+    );
+  });
