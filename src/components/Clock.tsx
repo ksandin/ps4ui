313dccb776cc1a13e72403163868e561bb8e87a2
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-19 17:36:10 +0200

    Remove unnecessary use of useObserver in Clock.tsx

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { Typography } from './Typography';
-import { useObserver } from 'mobx-react-lite';
 import { timeBehavior } from '../behaviors/timeBehavior';
 
 export const Clock = () => {
   const [time, setTime] = React.useState(new Date());
   React.useEffect(() => timeBehavior(setTime), []);
-  return useObserver(() => (
+  return (
     <Typography variant="h3">
       {time.getHours()}:{time.getMinutes()}
     </Typography>
-  ));
+  );
 };
