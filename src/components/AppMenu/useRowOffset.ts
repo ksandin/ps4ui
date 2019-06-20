1c2dffb56b6112c2bb2f875efdc9bab235004559
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-20 16:52:29 +0200

    Allow undefined refs in useRowOffset

--- src/components/AppMenu/useRowOffset.ts
+++ src/components/AppMenu/useRowOffset.ts
@@ -3,14 +3,14 @@ import { SpatialContext } from '../../lib/spatial/SpatialContext';
 import { useObserver } from 'mobx-react-lite';
 
 export const useRowOffset = <T extends any>(
-  rowRef: React.RefObject<HTMLElement>,
+  rowRef: React.RefObject<HTMLElement> | undefined,
   renderOffset: (offset: number) => React.ReactElement<T>
 ) => {
   const spatial = React.useContext(SpatialContext);
   return useObserver(() => {
     const active = spatial.getActive();
     const isContained =
-      active && rowRef.current && rowRef.current.contains(active);
+      rowRef && active && rowRef.current && rowRef.current.contains(active);
     const offset = isContained ? -active!.offsetLeft : 0;
     return renderOffset(offset);
   });
