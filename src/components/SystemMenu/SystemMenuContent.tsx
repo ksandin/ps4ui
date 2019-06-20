f8b3317c6baa63ed0480eb4c268a24cfb1310c5f
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-20 22:36:03 +0200

    Polish padding and text in SystemMenuContent

--- src/components/SystemMenu/SystemMenuContent.tsx
+++ src/components/SystemMenu/SystemMenuContent.tsx
@@ -12,10 +12,12 @@ export const SystemMenuContent: React.FC<SystemMenuContentProps> = ({
   system
 }) => (
   <Bounds>
-    {system && <Typography variant="h1">{system.name}</Typography>}
+    {system && <Typography variant="h3">{system.name}</Typography>}
   </Bounds>
 );
 
 const Bounds = styled.div`
   height: ${props => math(`${props.theme.unit} * 77`)};
+  padding: ${props => math(`${props.theme.unit} * 4`)}
+    ${props => math(`${props.theme.unit} * 12`)};
 `;
