e75cb6f4b75376037838a0567aed5b7693783d30
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-20 19:48:58 +0200

    Improve text formatting in clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import { Typography, TypographyProps } from './Typography';
 import { useTimer } from '../hooks/useTimer';
+import styled from 'styled-components';
 
 export type ClockProps = TypographyProps;
 
 export const Clock: React.FC<ClockProps> = props => {
   const time = useTimer();
   return (
-    <Typography variant="h3" {...props}>
-      {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
-    </Typography>
+    <ClockTypography {...props}>
+      {timeString(time.getHours())}:{timeString(time.getMinutes())}
+    </ClockTypography>
   );
 };
+
+const ClockTypography = styled(Typography).attrs<TypographyProps>({
+  variant: 'h3'
+})``;
+
+const timeString = (num: number) => num.toString().padStart(2, '0');
