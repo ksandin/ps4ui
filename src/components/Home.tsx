ea3dbd6afe7d882fbfc4021bf8a96d799cd3a122
Kasper Sandin <kasper.sandin@gmail.com>
2019-06-20 16:57:57 +0200

    Stub system and app menu content
    Fixes #4, #8

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,28 @@ import { Dock } from './Dock';
 import { Clock } from './Clock';
 import { SystemMenu } from './SystemMenu/SystemMenu';
 import { AppMenu } from './AppMenu/AppMenu';
+import { StateContext } from '../state/State';
+import { useSpatialIndex } from '../lib/spatial/useSpatialIndex';
 
-export const Home = () => (
-  <>
-    <SystemMenu />
-    <AppMenu />
-    <Dock variant="topRight">
-      <Clock />
-    </Dock>
-  </>
-);
+export const Home = () => {
+  const { apps, systems } = React.useContext(StateContext);
+  const appMenuRef = React.useRef(null);
+  const appIndex = useSpatialIndex(appMenuRef);
+  const app = apps[appIndex];
+
+  const systemMenuRef = React.useRef(null);
+  const systemIndex = useSpatialIndex(systemMenuRef);
+  const system = systems[systemIndex];
+
+  return (
+    <>
+      {system ? system.name : undefined}
+      <SystemMenu ref={systemMenuRef} items={systems} />
+      <AppMenu ref={appMenuRef} items={apps} />
+      {app ? app.name : undefined}
+      <Dock variant="topRight">
+        <Clock />
+      </Dock>
+    </>
+  );
+};
